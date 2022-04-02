7b6dac28051434847a82ba696f3ce512218ad591
chrislee1996 <prepchrislee@gmail.com>
2022-04-02 13:51:51 -0400

    show route for api works

--- app/routes/product_routes.js
+++ app/routes/product_routes.js
@@ -45,12 +45,12 @@ router.get('/products', (req, res, next) => {
 
 // SHOW
 // GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+router.get('/products/:id', (req, res, next) => {
 	// req.params.id will be set based on the `:id` in the route
 	Product.findById(req.params.id)
 		.then(handle404)
 		// if `findById` is succesful, respond with 200 and "example" JSON
-		.then((example) => res.status(200).json({ example: example.toObject() }))
+		.then((product) => res.status(200).json({ product: product.toObject() }))
 		// if an error occurs, pass it to the handler
 		.catch(next)
 })
